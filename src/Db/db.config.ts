375fcd68acb4bf1262ab66c90b6cd4170299ca94
Mihail OLARU <mihai_olaru@ymail.com>
2022-06-14 09:22:19 +0300

    static db connection properties

--- src/Db/db.config.ts
+++ src/Db/db.config.ts
@@ -1,9 +1,9 @@
 import { Client } from 'pg';
 
 export const client = new Client({
-	user: 'dbuser',
-	host: 'database.server.com',
-	database: 'mydb',
-	password: 'secretpassword',
-	port: 3211,
+	user: 'postgres',
+	host: 'localhost',
+	database: 'expressTest',
+	password: 'password',
+	port: 5432,
 });
